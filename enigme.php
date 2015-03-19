dd3dc3eae6e31ca5a18e2993cef0fb188a7d4d02
Alice <alice.maixent@gmail.com>
2015-03-19 18:39:02 +0100

    Modif ' de enigme.php
    Maintenant la page n'est plus accessible sans être connecté ! Youhou !

--- enigme.php
+++ enigme.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 require ('main.inc.php');
+if (empty($_SESSION['login'])) {
+    header("location: index.php?alert=deconnecte");
+}
 if (!empty($_POST)) {
     extract($_POST);
 }
