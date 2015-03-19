3afa442cf501be38f01c250b22ea14f55c326f18
mcarlier <maelle-carlier@hotmail.fr>
2015-03-19 12:38:45 +0100

    insert include

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<footer class="conditions">
+    <p>
+        Maëlle Carlier & Alice Maixent<br>
+        IMAC 2014-2015 <br>
+    </p>
+</footer>
---
+++ include/menu.php
@@ -0,0 +1,11 @@
+
+<header id="navbar" class="navbar-collapse collapse bar_sup">
+    <p>Bonjour Nom_utilisateur_connecté, vous avez *** points.</p>
+    <ul class="nav navbar-nav links">
+        <li> <a href="regles.php">Les règles</a></li>
+        <li> <a href="enigme.php">Énigme en cours</a></li>
+        <li> <a href="messagerie.php" class="nv_mess">Ma messagerie</a></li>
+        <li> <a href="creation_enigme.php">Proposer une énigme</a></li>
+        <li> <a href="#deconnexion">Déconnexion</a></li>
+    </ul>
+</header>
