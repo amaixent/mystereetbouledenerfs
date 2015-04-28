ced3afd781c068b5a03da116e140005a3a11fb16
Alice <alice.maixent@gmail.com>
2015-04-28 19:26:26 +0200

    Ajout <head> dossier include modif chaque page
    include du head dans chaque page comme c'était le même partout

--- historique.php
+++ historique.php
@@ -4,23 +4,7 @@ require ('main.inc.php');
 ?>
 <!DOCTYPE html>
 <html>
-    <head>	
-        <meta charset="utf-8"/>    
-        <title>Mystereetbouledenerf</title>
-        <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-
-        <link href="css/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-    </head>
+    <?php include("include/head.php");?>
     <body>
 
         <?php
